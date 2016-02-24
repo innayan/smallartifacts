4c9a39d1d0e45c4f9c312f0a86501e1ab5359740
innayan <admin@mail.ru>
2016-02-24 15:01:38 +0300

    twelve

---
+++ git12.cmd
@@ -0,0 +1,7 @@
+cd "E:\-\small artifacts"
+git checkout master
+git checkout -b twelve
+echo "xtttt5555666666666" >> "E:\-\small artifacts\9.txt" 	
+git add .
+git commit -m twelve
+git push origin twelve
