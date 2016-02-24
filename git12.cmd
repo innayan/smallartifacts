254213f42d28f03280a6b8b137dccb6dfc1a1870
innayan <admin@mail.ru>
2016-02-24 15:15:51 +0300

    twenty_four

--- git12.cmd
+++ git12.cmd
@@ -1,7 +1,7 @@
 cd "E:\-\small artifacts"
-rem git checkout master
-git checkout -b six_sux
+git checkout master
+git checkout -b twenty_four
 echo "xtttt5555666666666" >> "E:\-\small artifacts\9.txt" 	
 git add .
-git commit -m six_sux
-git push origin six_sux
+git commit -m twenty_four
+git push origin twenty_four
