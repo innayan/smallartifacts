500999c2dc1df2e913201c2596eae053ce6b95c0
innayan <admin@mail.ru>
2016-02-24 15:05:54 +0300

    sixteen

--- git12.cmd
+++ git12.cmd
@@ -1,7 +1,7 @@
 cd "E:\-\small artifacts"
 git checkout master
-git checkout -b fiveteen
+git checkout -b sixteen
 echo "xtttt5555666666666" >> "E:\-\small artifacts\9.txt" 	
 git add .
-git commit -m fiveteen
-git push origin fiveteen
+git commit -m sixteen
+git push origin sixteen
