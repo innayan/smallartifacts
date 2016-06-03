c53ede461f02eea07642301bec8202275b23b107
inna_yan@mail.ru <admin@mail.ru>
2016-06-03 13:14:35 +0300

    xxx

--- git12.cmd
+++ git12.cmd
@@ -1,7 +1,7 @@
 cd "E:\-\small artifacts"
 git checkout master
 git checkout -b xxx
-echo "xtttt5555666666666" >> "E:\-\small artifacts\9.txt" 	
+echo "xtttt5555666666666" >> "E:\-\small artifacts\asa.txt" 	
 git add .
 git commit -m xxx
 git push origin xxx
