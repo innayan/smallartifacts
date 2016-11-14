08fc882992bfc2aba1c70279f17006283f7fc416
innayan <inna_yan@mail.ru>
2016-11-14 13:29:17 +0300

    twelve_long_branch_name_to_experiment

--- git12.cmd
+++ git12.cmd
@@ -1,7 +1,7 @@
 cd "E:\-\small artifacts"
 git checkout master
-git checkout -b twelve_long_branch_name_to_experiment
+git checkout -b twelve_long_branch_name_to_experiment1
 echo "xtttt5555666666666" >> "E:\-\small artifacts\9.txt" 	
 git add .
 git commit -m twelve_long_branch_name_to_experiment
-git push origin 
+git push origin twelve_long_branch_name_to_experiment1
